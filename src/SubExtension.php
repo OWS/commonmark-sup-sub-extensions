b39fbb919a47de8d8b17d296c9fc9ec090ab37be
pazairfog <pazairfog@users.noreply.github.com>
2019-09-27 18:09:44 +0200

    Refactoring Sub extension

--- src/SubExtension.php
+++ src/SubExtension.php
@@ -10,31 +10,27 @@
 
 namespace Ows\CommonMark;
 
+use League\CommonMark\ConfigurableEnvironmentInterface;
+use League\CommonMark\Extension\ExtensionInterface;
+use Ows\CommonMark\Inline\Element\Sub;
 use Ows\CommonMark\Inline\Parser\SubParser;
 use Ows\CommonMark\Inline\Renderer\SubRenderer;
-use League\CommonMark\Extension\Extension;
 
-class SubExtension extends Extension
+final class SubExtension implements ExtensionInterface
 {
 
   /**
    * {@inheritdoc}
    */
-  public function getInlineParsers()
-  {
-    return [
-      new SubParser(),
-    ];
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getInlineRenderers()
-  {
-    return [
-      'Ows\CommonMark\Inline\Element\Sub' => new SubRenderer(),
-    ];
+  public function register(ConfigurableEnvironmentInterface $environment) {
+    $environment
+      ->addInlineParser(new SubParser())
+      ->addInlineRenderer(
+        Sub::class,
+        new SubRenderer()
+      )
+    ;
   }
 
 }
+
