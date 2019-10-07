75c6a42c54f2d2ca7179713bf3a614674a2311ed
pazairfog <pazairfog@users.noreply.github.com>
2019-10-07 15:10:21 +0200

    Add @deprecated docblock for markdown extensions

--- deprecated/SubExtension.php
+++ deprecated/SubExtension.php
@@ -4,6 +4,11 @@ namespace Ows\CommonMark;
 
 use Ows\CommonMark\SupSubExtensions\SubExtension as BaseSubExtension;
 
+/**
+ * Add subscript (<sub> tag) to league/commonmark.
+ *
+ * @deprecated in branch 2.x, will be removed before 3.0.0.
+ */
 final class SubExtension extends BaseSubExtension
 {
 
--- deprecated/SupExtension.php
+++ deprecated/SupExtension.php
@@ -4,6 +4,11 @@ namespace Ows\CommonMark;
 
 use Ows\CommonMark\SupSubExtensions\SupExtension as BaseSupExtension;
 
+/**
+ * Add superscript (<sup> tag) to league/commonmark.
+ *
+ * @deprecated in branch 2.x, will be removed before 3.0.0.
+ */
 final class SupExtension extends BaseSupExtension
 {
 
